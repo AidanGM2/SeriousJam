6e7ae92f791dafe47be0d3f8d8f654d7690fb8a0
AidanGM2 <agmellon@gmail.com>
2026-06-24 20:25:24 -0500

    Basic enemy detection
    Will be edited I just wanted to pull the most recent updates to enemy behaviour

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -3,21 +3,81 @@ using UnityEngine;
 public class BasicEnemy : MonoBehaviour
 {
 
-    public Transform player;
+    [SerializeField]
+    private float enemySpeed;
+
+
+    [SerializeField]
+    private float rotationSpeed;
+
+
+
+    // public Transform player;
     private Rigidbody2D rb;
+    private PlayerAwareness playerAwareness;
+    private Vector2 targetDirection;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        playerAwareness = GetComponent<PlayerAwareness>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 direction = player.position = transform.position;
+
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+
+        /*
+        Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
+        */
+    }
+
+
+    private void UpdateTargetDirection()
+    {
+        if (playerAwareness.AwareOfPlayer)
+        {
+            targetDirection = playerAwareness.DirectionToPlayer;
+        } else
+        {
+            targetDirection = Vector2.zero;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if(targetDirection == Vector2.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        rb.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            rb.linearVelocity = transform.up * enemySpeed;
+        }
+
 
     }
+
+
+
 }
---
+++ Assets/Scripts/PlayerAwareness.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerAwareness : MonoBehaviour
+{
+
+    public bool AwareOfPlayer { get; private set; }
+
+
+    public Vector2 DirectionToPlayer {  get; private set; }
+
+    [SerializeField]
+    private float playerAwarenessDistance;
+
+
+    private Transform player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        player = FindAnyObjectByType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemyToPlayerVector = player.position - transform.position;
+        DirectionToPlayer = enemyToPlayerVector.normalized;
+
+
+        if (enemyToPlayerVector.magnitude <= playerAwarenessDistance)
+        {
+            AwareOfPlayer = true;
+        } else
+        {
+            AwareOfPlayer= false;
+        }
+
+    }
+}
