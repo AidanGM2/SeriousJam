54b686f714b51d4b5986817e882a9baa09cb1a6e
AidanGM2 <agmellon@gmail.com>
2026-06-26 23:51:29 -0500

    oihhiujnhbiu

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -5,10 +5,15 @@ public class MainMenuButtons : MonoBehaviour
 {
    
     [SerializeField] private string startLevel = "Level";
-
+    [SerializeField] private string credits = "CreditsScreen";
     public void StartButton()
     {
         SceneManager.LoadScene(startLevel);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(credits);
     }
 
     public void quitGame()
